4a9b4857cfa1ce39a2dd1c2d01e55d544a84d06e
Brandon Swift <bswuft@gmail.com>
2012-06-14 17:34:31 +0000

    gave post data priority over get

--- library/Swyft/SRequest.php
+++ library/Swyft/SRequest.php
@@ -12,34 +12,47 @@ class SRequest{
 	* Retrieve request data and organize
 	*/
 	function __construct(){
-		foreach(@$_POST AS $key => $value){
-			$this->posts[$key] = mysql_real_escape_string($value);
+		foreach(@$_REQUEST AS $key => $value){
+			$this->gets[$key] = mysql_real_escape_string($value);
 			$this->params[$key] = mysql_real_escape_string($value);
 		}
 		foreach(@$_GET AS $key => $value){
 			$this->gets[$key] = mysql_real_escape_string($value);
 			$this->params[$key] = mysql_real_escape_string($value);
 		}
-		
-		foreach(@$_REQUEST AS $key => $value){
-			$this->gets[$key] = mysql_real_escape_string($value);
+		foreach(@$_POST AS $key => $value){
+			$this->posts[$key] = mysql_real_escape_string($value);
 			$this->params[$key] = mysql_real_escape_string($value);
 		}
 		
 	}
 	
+	/**
+	* Get post data
+	*/
 	public function getPosts(){
 		return $this->posts;
 	}
 	
+	/**
+	* Get request data
+	*/
 	public function getRequests(){
 		return $this->gets;
 	}
 	
+	/**
+	* Get post and request data
+	*/
 	public function getParams(){
 		return $this->params;
 	}
 	
+	/**
+	* Get post data
+	* @param $key 
+	* @param $default Default value if not set
+	*/
 	public function getPost($key,$default=null){
 		if($this->posts[$key]){
 			return $this->posts[$key];
@@ -47,7 +60,11 @@ class SRequest{
 			return $default;
 		}
 	}
-	
+	/**
+	* Get request data
+	* @param $key 
+	* @param $default Default value if not set
+	*/
 	public function getRequest($key,$default=null){
 		if($this->gets[$key]){
 			return $this->gets[$key];
@@ -56,6 +73,11 @@ class SRequest{
 		}
 	}
 	
+	/**
+	* Get post and request data
+	* @param $key 
+	* @param $default Default value if not set
+	*/
 	public function getParam($key,$default=null){
 		if($this->params[$key]){
 			return $this->params[$key];
